1cc2cb5b70a980086c605255ab92bec3e9a26906
Uttam-94 <uttamskk1@gmail.com>
2025-11-06 14:59:14 +0530

    Functional Interfaces

--- Lambda-Expressions/Name-Uppercasing.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class NameUppercasing {
-    public static void main(String[] args){
-        List<String> names = Arrays.asList("ravi","meena","arjun","sana");
-        names.stream().map(String::toUpperCase).forEach(System.out::println);
-    }
-}
